a77d8e4614e78bd489d796405ef25412c5c82dd4
ferkei <101744143+ferkei@users.noreply.github.com>
2023-11-22 09:53:20 -0600

    Add files via upload

---
+++ assets/php/correo.php
@@ -0,0 +1,10 @@
+<?PHP
+
+$destino = "seguridad.jbh@gmail.com";
+    	$Nombre = $_POST['Nombre'];
+        $Email = $_POST['Email'];
+        $Msg = $_POST ['Msg'];
+	$contenido = "Nombre: " . $Nombre . "\n Email: " . $Email. "\n Msg: " . $Msg;
+	mail($destino, "Contacto", $contenido);
+	header("Location: #contact");
+?>
